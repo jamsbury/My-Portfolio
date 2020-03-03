12d81e9a93248217d32c11b367b85fcb63f44a10
Justice Amsbury <35708932+jamsbury@users.noreply.github.com>
2020-03-02 18:33:48 -0800

    Google Console

--- js/scripts.js
+++ js/scripts.js
@@ -90,22 +90,6 @@ $(document).ready(function() {
 });
 
 
-// Hotjar tracking script for testing and issue identification
-
-(function(h, o, t, j, a, r) {
-  h.hj =
-    h.hj ||
-    function() {
-      (h.hj.q = h.hj.q || []).push(arguments);
-    };
-  h._hjSettings = { hjid: 1265298, hjsv: 6 };
-  a = o.getElementsByTagName("head")[0];
-  r = o.createElement("script");
-  r.async = 1;
-  r.src = t + h._hjSettings.hjid + j + h._hjSettings.hjsv;
-  a.appendChild(r);
-})(window, document, "https://static.hotjar.com/c/hotjar-", ".js?sv=");
-
 // Featherlight Lightbox config
 
 $.extend($.featherlight.defaults, {
